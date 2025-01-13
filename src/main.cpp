8fc02d886b3c8d4710d4df3df4538daaed389162
installg3ntoo <fiquintero@uc.cl>
2025-01-12 22:43:37 -0300

    Update .gitignore and main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdlib>
+#include <limits> 
 #include "tablero.h"
 
 
@@ -11,20 +13,39 @@ using namespace std;
 
 int main() {
 
-    int opcion {0};
+    int opcion {-1};
+    int N {7}, M {20};
+    cout << N << " " << M << endl;
+    
+    Tablero tablero {N, M};
+
+    cout <<"hola como estás " <<endl;
+
+    print_tablero_con_utf8(tablero.tablero);
 
     std::cout << "\u001b[33mBienvenido A Lego Sweeper!\n\n"
-              << "\u001b[34mPorfavor escoja una opción\n"
+              << u8"\u001b[34mPorfavor escoja una opción\n"
               << "\u001b[0m"
               << endl;
-    while (opcion != 3) {
-        cout << "1. Jugar" << endl;
-        cout << "2. Ver instrucciones" << endl;
-        cout << "3. Salir" << endl;
-        cout << "Ingrese una opción: ";
+    
+    while (opcion != 0) {
+        cout
+            << u8"Seleccione una opción:" << endl
+            << "\u001b[32m1)Comenzar una nueva partida" << endl
+            << "\u001b[33m2)Cargar partida existente" << endl
+            << "\u001b[35m3)Ver el ranking de puntajes" << endl
+            << "\u001b[36;1m0)Salir\u001b[0m" << endl;
+
         cin >> opcion;
 
+        if (cin.fail()) {
+            cin.clear(); // Clear the error state
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            opcion = -1;
+        }
+
         switch (opcion) {
+            
             case 1:
                 cout << "Jugando..." << endl;
                 break;
@@ -34,8 +55,16 @@ int main() {
             case 3:
                 cout << "Saliendo..." << endl;
                 break;
+            case 0:
+                cout << "Saliendo..." << endl;
+                break;
             default:
-                cout << "Opción inválida" << endl;
+                #ifdef _WIN32
+                    system("cls");  // Windows
+                #else
+                    system("clear");  // Linux/Mac
+                #endif
+                cout << u8"\u001b[31mCarácter incorrecto, porfavor escriba el número de la opción deseada" << endl;
                 break;
         }
     }
